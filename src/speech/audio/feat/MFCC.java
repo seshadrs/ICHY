8fc533d8084c0b7ffa1456e34789e2c28bb05408
Seshadri Sridharan <mit.seshadri@gmail.com>
2013-03-06 12:54:14 -0500

    Fixed MFCC.java to remove Infinity, Nan values. Fixed MFCC.java to calculate acceleration features correctly. Modified recording names for digit 1.

--- src/speech/audio/feat/MFCC.java
+++ src/speech/audio/feat/MFCC.java
@@ -2,6 +2,8 @@ package speech.audio.feat;
 
 import java.io.IOException;
 
+import sun.awt.SunToolkit.InfiniteLoop;
+
 import comirva.audio.feature.*;
 
 /*
@@ -40,28 +42,28 @@ public class MFCC {
 	 //Returns the 13 LogMel features, the velocity and the acceleration of the features.
 	 public double[][] extractAll39Features(double[] data) throws IllegalArgumentException, IOException
 	 {
-		 double[][] logMelVectors = extractor.processLogMel(data);
-		 double[][] allFeats = new double[logMelVectors.length][39];
+		 double[][] featureVectors = extractor.process(data);
+		 double[][] allFeats = new double[featureVectors.length][39];
 		 
 		 double[] velocityVector= new double[13];
 		 double[] prevVelocityVector= new double[13];
 		 double[] accelerationVector= new double[13];
-		 double[] prevLogMelVector = new double[13];
-		
-		 int pos=0;
+		 double[] prevFeatVector = new double[13];
+		 
+		  int pos=0;
 		 double velI,accI;
 		 
-		 for(double[] logMelVector : logMelVectors)
+		 for(double[] featVector : featureVectors)
 		 {
 			 //enter values into new feat-arr
 			 for(int i=0; i<13;i++)
-				 allFeats[pos][i] = logMelVector[i];
+				 allFeats[pos][i] = featVector[i];
 			 
 			 if(pos>0)	//calculate velocity
 			 {
 				 for(int i=0; i<13;i++)
 					 {
-					 	velI = logMelVector[i]-prevLogMelVector[i];
+					 	velI = featVector[i]-prevFeatVector[i];
 					 	allFeats[pos][13+i] = velI;
 					 	velocityVector[i] = velI; 
 					 }
@@ -78,13 +80,51 @@ public class MFCC {
 			 }
 			 
 			 pos+=1;
-			 prevLogMelVector = logMelVector;
-			 prevVelocityVector = velocityVector;
+			 prevFeatVector = featVector;
+			 prevVelocityVector = velocityVector.clone();
 				 
 			 
 		 }
 		 
+		 //System.out.println("before "+allFeats.length);
+		 allFeats = filterPrefixInfinityFrames(allFeats);
+		 //System.out.println("after "+allFeats.length);
+		 
 		return allFeats; 
 	 }
+
+	private double[][] filterPrefixInfinityFrames(double[][] allFeats) {
+		
+		//scan first to find out the number of prefix rows with -Infinity
+		int prefixRowsToRemove=0;
+		for(int i=0; i<allFeats.length; i++)
+		{
+			boolean removeRow = false;
+			
+			for(int j=0; j<allFeats[i].length; j++)
+			{
+				if (allFeats[i][j] == Double.NEGATIVE_INFINITY || allFeats[i][j] == Double.POSITIVE_INFINITY || allFeats[i][j] == Double.NaN)
+					{
+						removeRow = true;
+					}
+			}
+			
+			if (removeRow)
+				prefixRowsToRemove +=1;
+			else
+				break;
+		}
+		
+		//now remove hte prefix rows and return rest of 2D arr
+		double[][] filteredFeatures = new double[allFeats.length-prefixRowsToRemove][39];
+		for(int i=0; i< filteredFeatures.length; i++)
+		{
+			for(int j=0; j<39;j++)
+				filteredFeatures[i][j] = allFeats[i+prefixRowsToRemove][j];
+		}
+		
+		return filteredFeatures;
+		
+	}
 
 }

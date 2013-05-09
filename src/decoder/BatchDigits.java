b6e3c1d6a223c0c2986dbd5f830bc9c4bed6c1b5
Sukhada Palkar <sukhada.palkar@gmail.com>
2013-05-09 03:05:56 -0400

    removed bad import

--- src/decoder/BatchDigits.java
+++ src/decoder/BatchDigits.java
@@ -16,8 +16,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
 
-import javax.media.jai.operator.MaxDescriptor;
-
 import models.LM;
 import models.phoneme.Gaussian;
 import models.phoneme.HMM;
